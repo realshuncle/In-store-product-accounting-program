b2cf58d9db439d62c1e8b490820c3a5783cf7ef6
realshuncle <realshuncle@gmail.com>
2021-08-02 17:10:43 +0400

    A method for adding a product has been created.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,10 +5,29 @@ namespace FirstApp
 {
     class Program
     {
+        static void Added(object sender, EnterpriseEventsArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        static void Add(Shop shop)
+        {
+            String name, shelfLife;
+            int amount;
+            Console.WriteLine("Enter product name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the expiration date of the product");
+            shelfLife = Console.ReadLine();
+            Console.WriteLine("Enter the amount of the product");
+            amount = Convert.ToInt32(Console.ReadLine());
+            Product product = new(name, amount, shelfLife);
+            shop.AddProduct(product);
+        }
         static void Main(string[] args)
         {
-            Shop a;
+            EnterpriseHandler added = Added;
+            Shop a = new Shop(Added, Added, Added, Added);
             Console.WriteLine("Hello World!");
+            Add(a);
         }
     }
 }
