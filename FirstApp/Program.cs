76977df73b4741665067c7a6d72eef1df1a1e7d4
realshuncle <realshuncle@gmail.com>
2021-08-03 12:41:46 +0400

    The Remove method has been implemented.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -52,11 +52,39 @@ namespace FirstApp
                 Console.WriteLine(e.Message);
             }  
         }
+        static void Remove(Shop shop)
+        {
+            String name, shelfLife;
+            int amount;
+            Console.WriteLine("Enter product name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the expiration date of the product");
+            shelfLife = Console.ReadLine();
+            Console.WriteLine("Enter the amount of the product");
+            try
+            {
+                amount = Convert.ToInt32(Console.ReadLine());
+                Product product = new(name, amount, shelfLife);
+                try
+                {
+                    shop.RemoveProduct(product);
+                }
+                catch (EnterpriseException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
         static void Main(string[] args)
         {
             Shop a = new Shop(Added, Removed, Added, Added);
             Console.WriteLine("Hello World!");
             Add(a);
+            Remove(a);
         }
     }
 }
